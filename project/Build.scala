764c3b1b7f9cf0975ffba5d8a27bc19eec58c554
myyk <myyk@egraphs.com>
2012-09-12 11:26:51 -0700

    Moved all the code to root and deleted the sample.

---
+++ project/Build.scala
@@ -0,0 +1,16 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "play2-authenticitytoken"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq()
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+		organization := "egraphs"
+    )
+
+}
